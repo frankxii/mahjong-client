a9382ea55f909f93056177f578473ac8221124aa
frank <frankw.wang@qq.com>
2022-09-08 15:45:05 +0800

    feat: 添加创建游戏房间功能

---
+++ Assets/Scripts/UI/CreateRoom.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateRoom : MonoBehaviour
+{
+    public ToggleGroup chiToggleGroup;
+    public ToggleGroup gameCycleToggleGroup;
+    public Text diamondCostNumber;
+    public Button generalModeCreateBtn;
+
+    private void Awake()
+    {
+        generalModeCreateBtn.onClick.AddListener(CreateGeneralModeRoom);
+        // 给所有圈数开关挂上请求钻石消耗回调。 
+        foreach (Transform toggle in gameCycleToggleGroup.transform)
+        {
+            toggle.GetComponent<Toggle>().onValueChanged.AddListener(OnGameCycleChange);
+        }
+    }
+
+    private void Start()
+    {
+        // 初始化钻石开销
+        UpdateDiamondCostNumber();
+    }
+
+    // 更改游戏圈数后，更新钻石开销
+    public void OnGameCycleChange(bool isOn)
+    {
+        if (isOn)
+        {
+            UpdateDiamondCostNumber();
+        }
+    }
+
+    // 获取游戏圈数规则
+    private int GetGameCycleNumber()
+    {
+        Toggle selectedToggle;
+        // 获取游戏圈数
+        selectedToggle = gameCycleToggleGroup.ActiveToggles().ToList()[0];
+        int gameCycle = 4;
+        if (selectedToggle.name == "SixteenCycleToggle")
+            gameCycle = 16;
+        else if (selectedToggle.name == "EightCycleToggle")
+            gameCycle = 8;
+        else if (selectedToggle.name == "FourCycleToggle")
+            gameCycle = 4;
+        return gameCycle;
+    }
+
+    // 创建房间
+    private void CreateGeneralModeRoom()
+    {
+        Toggle selectedToggle = chiToggleGroup.ActiveToggles().ToList()[0];
+        // 是否允许吃
+        bool hasChi = selectedToggle.name == "CanChiToggle";
+        int gameCycleNumber = GetGameCycleNumber();
+    }
+
+    // 更新钻石开销文本
+    public void UpdateDiamondCostNumber()
+    {
+        int gameCycleNumber = GetGameCycleNumber();
+        diamondCostNumber.text = gameCycleNumber.ToString();
+    }
+}
--- Assets/Scripts/UI/Login.cs
+++ Assets/Scripts/UI/Login.cs
@@ -10,11 +10,14 @@ public class Login : MonoBehaviour
     public TMP_InputField username;
     public Button loginBtn;
 
-    private void Start()
+
+    private void Awake()
     {
         loginBtn.onClick.AddListener(OnClick);
     }
 
+
+    // 登录按钮回调
     private void OnClick()
     {
         int id = Convert.ToInt32(username.text);
